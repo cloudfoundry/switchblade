e67287285829045ac2e7b3d290f6e4ebe9ae2cad
Ryan Moran <rmoran@vmware.com>
2022-09-06 14:41:33 -0700

    Initialize buildpacks with delete/recreate

--- internal/cloudfoundry/initialize_test.go
+++ internal/cloudfoundry/initialize_test.go
@@ -30,6 +30,16 @@ func testInitialize(t *testing.T, context spec.G, it spec.S) {
 			executable = &fakes.Executable{}
 			executable.ExecuteCall.Stub = func(execution pexec.Execution) error {
 				executions = append(executions, execution)
+
+				args := strings.Join(execution.Args, " ")
+				switch {
+				case strings.Contains(args, "curl /v3/buildpacks?names=some-buildpack-name"):
+					fmt.Fprint(execution.Stdout, `{"resources":[{"position": 123}]}`)
+
+				case strings.Contains(args, "curl /v3/buildpacks?names=other-buildpack-name"):
+					fmt.Fprint(execution.Stdout, `{"resources":[{"position": 234}]}`)
+				}
+
 				return nil
 			}
 
@@ -49,12 +59,24 @@ func testInitialize(t *testing.T, context spec.G, it spec.S) {
 			})
 			Expect(err).NotTo(HaveOccurred())
 
-			Expect(executions).To(HaveLen(2))
+			Expect(executions).To(HaveLen(6))
 			Expect(executions[0]).To(MatchFields(IgnoreExtras, Fields{
-				"Args": Equal([]string{"update-buildpack", "some-buildpack-name", "-p", "some-buildpack-uri"}),
+				"Args": Equal([]string{"curl", "/v3/buildpacks?names=some-buildpack-name"}),
 			}))
 			Expect(executions[1]).To(MatchFields(IgnoreExtras, Fields{
-				"Args": Equal([]string{"update-buildpack", "other-buildpack-name", "-p", "other-buildpack-uri"}),
+				"Args": Equal([]string{"delete-buildpack", "-f", "some-buildpack-name"}),
+			}))
+			Expect(executions[2]).To(MatchFields(IgnoreExtras, Fields{
+				"Args": Equal([]string{"create-buildpack", "some-buildpack-name", "some-buildpack-uri", "123"}),
+			}))
+			Expect(executions[3]).To(MatchFields(IgnoreExtras, Fields{
+				"Args": Equal([]string{"curl", "/v3/buildpacks?names=other-buildpack-name"}),
+			}))
+			Expect(executions[4]).To(MatchFields(IgnoreExtras, Fields{
+				"Args": Equal([]string{"delete-buildpack", "-f", "other-buildpack-name"}),
+			}))
+			Expect(executions[5]).To(MatchFields(IgnoreExtras, Fields{
+				"Args": Equal([]string{"create-buildpack", "other-buildpack-name", "other-buildpack-uri", "234"}),
 			}))
 		})
 
@@ -63,8 +85,13 @@ func testInitialize(t *testing.T, context spec.G, it spec.S) {
 				executable.ExecuteCall.Stub = func(execution pexec.Execution) error {
 					executions = append(executions, execution)
 
-					if strings.Contains(strings.Join(execution.Args, " "), "update-buildpack some-buildpack-name") {
+					args := strings.Join(execution.Args, " ")
+					switch {
+					case strings.Contains(args, "curl /v3/buildpacks?names=some-buildpack-name"):
 						return errors.New("no such buildpack")
+
+					case strings.Contains(args, "curl /v3/buildpacks?names=other-buildpack-name"):
+						fmt.Fprint(execution.Stdout, `{"resources":[{"position": 234}]}`)
 					}
 
 					return nil
@@ -84,49 +111,123 @@ func testInitialize(t *testing.T, context spec.G, it spec.S) {
 				})
 				Expect(err).NotTo(HaveOccurred())
 
-				Expect(executions).To(HaveLen(3))
+				Expect(executions).To(HaveLen(5))
 				Expect(executions[0]).To(MatchFields(IgnoreExtras, Fields{
-					"Args": Equal([]string{"update-buildpack", "some-buildpack-name", "-p", "some-buildpack-uri"}),
+					"Args": Equal([]string{"curl", "/v3/buildpacks?names=some-buildpack-name"}),
 				}))
 				Expect(executions[1]).To(MatchFields(IgnoreExtras, Fields{
 					"Args": Equal([]string{"create-buildpack", "some-buildpack-name", "some-buildpack-uri", "1000"}),
 				}))
 				Expect(executions[2]).To(MatchFields(IgnoreExtras, Fields{
-					"Args": Equal([]string{"update-buildpack", "other-buildpack-name", "-p", "other-buildpack-uri"}),
+					"Args": Equal([]string{"curl", "/v3/buildpacks?names=other-buildpack-name"}),
+				}))
+				Expect(executions[3]).To(MatchFields(IgnoreExtras, Fields{
+					"Args": Equal([]string{"delete-buildpack", "-f", "other-buildpack-name"}),
+				}))
+				Expect(executions[4]).To(MatchFields(IgnoreExtras, Fields{
+					"Args": Equal([]string{"create-buildpack", "other-buildpack-name", "other-buildpack-uri", "234"}),
 				}))
 			})
 		})
 
-		context("when a buildpack cannot be created", func() {
-			it.Before(func() {
-				executable.ExecuteCall.Stub = func(execution pexec.Execution) error {
-					executions = append(executions, execution)
+		context("failure cases", func() {
+			context("when the buildpack JSON cannot be parsed", func() {
+				it.Before(func() {
+					executable.ExecuteCall.Stub = func(execution pexec.Execution) error {
+						executions = append(executions, execution)
 
-					if strings.Contains(strings.Join(execution.Args, " "), "update-buildpack some-buildpack-name") {
-						return errors.New("no such buildpack")
+						args := strings.Join(execution.Args, " ")
+						switch {
+						case strings.Contains(args, "curl /v3/buildpacks?names=some-buildpack-name"):
+							fmt.Fprint(execution.Stdout, "%%%")
+						}
+
+						return nil
 					}
+				})
 
-					if strings.Contains(strings.Join(execution.Args, " "), "create-buildpack some-buildpack-name") {
-						fmt.Fprintln(execution.Stderr, "something bad happened")
-						return errors.New("create-buildpack failed")
-					}
-
-					return nil
-				}
+				it("returns an error", func() {
+					err := initialize.Run([]cloudfoundry.Buildpack{
+						{
+							Name: "some-buildpack-name",
+							URI:  "some-buildpack-uri",
+						},
+						{
+							Name: "other-buildpack-name",
+							URI:  "other-buildpack-uri",
+						},
+					})
+					Expect(err).To(MatchError(ContainSubstring("failed to parse buildpacks")))
+					Expect(err).To(MatchError(ContainSubstring("invalid character '%'")))
+				})
 			})
 
-			it("creates the buildpack", func() {
-				err := initialize.Run([]cloudfoundry.Buildpack{
-					{
-						Name: "some-buildpack-name",
-						URI:  "some-buildpack-uri",
-					},
-					{
-						Name: "other-buildpack-name",
-						URI:  "other-buildpack-uri",
-					},
+			context("when a buildpack cannot be deleted", func() {
+				it.Before(func() {
+					executable.ExecuteCall.Stub = func(execution pexec.Execution) error {
+						executions = append(executions, execution)
+
+						args := strings.Join(execution.Args, " ")
+						switch {
+						case strings.Contains(args, "curl /v3/buildpacks?names=some-buildpack-name"):
+							fmt.Fprintln(execution.Stdout, "{}")
+
+						case strings.Contains(args, "delete-buildpack -f some-buildpack-name"):
+							fmt.Fprintln(execution.Stderr, "something bad happened")
+							return errors.New("delete-buildpack failed")
+						}
+
+						return nil
+					}
 				})
-				Expect(err).To(MatchError("failed to create buildpack: create-buildpack failed\n\nOutput:\nsomething bad happened\n"))
+
+				it("returns an error", func() {
+					err := initialize.Run([]cloudfoundry.Buildpack{
+						{
+							Name: "some-buildpack-name",
+							URI:  "some-buildpack-uri",
+						},
+						{
+							Name: "other-buildpack-name",
+							URI:  "other-buildpack-uri",
+						},
+					})
+					Expect(err).To(MatchError("failed to delete buildpack: delete-buildpack failed\n\nOutput:\n{}\nsomething bad happened\n"))
+				})
+			})
+
+			context("when a buildpack cannot be created", func() {
+				it.Before(func() {
+					executable.ExecuteCall.Stub = func(execution pexec.Execution) error {
+						executions = append(executions, execution)
+
+						args := strings.Join(execution.Args, " ")
+						switch {
+						case strings.Contains(args, "curl /v3/buildpacks?names=some-buildpack-name"):
+							return errors.New("no such buildpack")
+
+						case strings.Contains(args, "create-buildpack some-buildpack-name"):
+							fmt.Fprintln(execution.Stderr, "something bad happened")
+							return errors.New("create-buildpack failed")
+						}
+
+						return nil
+					}
+				})
+
+				it("returns an error", func() {
+					err := initialize.Run([]cloudfoundry.Buildpack{
+						{
+							Name: "some-buildpack-name",
+							URI:  "some-buildpack-uri",
+						},
+						{
+							Name: "other-buildpack-name",
+							URI:  "other-buildpack-uri",
+						},
+					})
+					Expect(err).To(MatchError("failed to create buildpack: create-buildpack failed\n\nOutput:\nsomething bad happened\n"))
+				})
 			})
 		})
 	})
